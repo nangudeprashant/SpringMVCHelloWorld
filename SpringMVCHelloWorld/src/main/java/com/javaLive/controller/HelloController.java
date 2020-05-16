be9e6c97a122244d797f23e9735abb3d1349edad
Administrator <Administrator@DELL>
2020-05-16 19:42:33 +0530

    Some modifications are made.

--- SpringMVCHelloWorld/src/main/java/com/javaLive/controller/HelloController.java
+++ SpringMVCHelloWorld/src/main/java/com/javaLive/controller/HelloController.java
@@ -1,5 +1,13 @@
 package com.javaLive.controller;
 
-public class HelloController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-}
+@Controller  
+public class HelloController {  
+@RequestMapping("/")  
+    public String display()  
+    {  
+        return "index";  
+    }     
+}  
