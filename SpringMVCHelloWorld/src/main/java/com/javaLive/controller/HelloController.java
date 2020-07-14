440770084d4ad2aee64b064a9625aec1ec876cc1
Administrator <Administrator@DELL>
2020-07-14 19:27:49 +0530

    Project guide file added and some functionality modification made.

--- SpringMVCHelloWorld/src/main/java/com/javaLive/controller/HelloController.java
+++ SpringMVCHelloWorld/src/main/java/com/javaLive/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller  
 public class HelloController {  
-@RequestMapping("/")  
+@RequestMapping("/index")  
     public String display()  
     {  
         return "index";  
